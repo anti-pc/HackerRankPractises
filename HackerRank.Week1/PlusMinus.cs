6ff075d3583d95f960dbf46dd197261fe956eb79
buketculfaoglu <buketculfaoglu@gmail.com>
2024-05-20 19:20:26 +0300

    Add project files.

---
+++ HackerRank.Week1/PlusMinus.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.Week1
+{
+    public static class PlusMinus
+    {
+        public static void plusMinus(List<int> arr)
+        {
+            double numOfPositive = 0;
+            double numOfNegative = 0;
+            double numOfZero = 0;
+            foreach (int item in arr)
+            {
+                if (item == 0)
+                    numOfZero++;
+                if (item > 0)
+                    numOfPositive++;
+                if (item < 0)
+                    numOfNegative++;
+            }
+
+            Console.WriteLine(numOfPositive / arr.Count);
+            Console.WriteLine(numOfNegative / arr.Count);
+            Console.WriteLine(numOfZero / arr.Count);
+
+        }
+    }
+}
---
+++ HackerRank.Week1/Program.cs
@@ -0,0 +1,7 @@
+﻿using HackerRank.Week1;
+
+int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+PlusMinus.plusMinus(arr);
