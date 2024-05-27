0b5f4bcba805df86ed4fc2a9b4096dba70f6534b
buketculfaoglu <buketculfaoglu@gmail.com>
2024-05-27 16:07:23 +0300

    LonelyInteger challenge

---
+++ HackerRank.Week1/LonelyInteger.cs
@@ -0,0 +1,23 @@
+﻿namespace HackerRank.Week1
+{
+    internal static class LonelyInteger
+    {
+        public static int lonelyinteger(List<int> a)
+        {
+            if (a.Count == 1)
+                return a[0];
+
+            foreach (var item in a)
+            {
+                if (a.Count(x => x == item) > 1)
+                    continue;
+
+                else return item;
+            }
+
+            return 0;
+        }
+
+
+    }
+}
--- HackerRank.Week1/Program.cs
+++ HackerRank.Week1/Program.cs
@@ -1,6 +1,12 @@
 ﻿using HackerRank.Week1;
 using System.Collections.Generic;
 
+// LonelyInteger
+
+List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+Console.WriteLine(LonelyInteger.lonelyinteger(a));
+
+/*
 
 // SparseArrays
 
@@ -11,7 +17,7 @@ List<string> queries = Console.ReadLine().TrimEnd().Split(' ').ToList();
 List<int> result = SparseArrays.matchingStrings(strings, queries);
 
 result.ForEach(i => Console.Write("{0}\t", i));
-/*
+
 
 //TimeConversion
 
