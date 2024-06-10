19b4fec61c833605d0e84ae56bdd421c2ae756f2
buketculfaoglu <buketculfaoglu@gmail.com>
2024-06-10 19:18:53 +0300

    Counting Sort algorithm

---
+++ HackerRank.Week1/CountingSort.cs
@@ -0,0 +1,18 @@
+﻿
+namespace HackerRank.Week1
+{
+    public static class CountingSort
+    {
+        public static List<int> countingSort(List<int> arr)
+        {
+            int[] count = new int[100];
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                count[arr[i]]++;
+            }
+
+            return count.ToList();
+        }
+    }
+}
